44c80e5114122aa9852f63ceb4eee039f0b4b099
SayedSaberAmeen <alsayedelbarbary959@gmail.com>
2023-05-10 02:50:08 -0700

    role repository commit 4

--- import project/company/src/main/java/com/testProject/demo/repository/RolesRepository.java
+++ import project/company/src/main/java/com/testProject/demo/repository/RolesRepository.java
@@ -1,11 +1,12 @@
 package com.testProject.demo.repository;
 
-import com.testProject.demo.common.entity.Employee;
+
+import com.testProject.demo.common.entity.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-    public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface RolesRepository extends JpaRepository<Roles , Integer> {
 
 
 }
